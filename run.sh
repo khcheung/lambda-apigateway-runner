fbe304c62b118cdad166cfc78fe67bbe2dc99251
Alex Cheung <alexcheung1980@hotmail.com>
2023-01-05 23:29:38 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 dotnet run
 cd /opt/console
-#dotnet run -r linux-x64 -c Release --no-self-contained --urls "http://+:80"
-#dotnet run -r linux-x64 -c Release --sc --urls "http://+:80"
-dotnet run --runtime linux-x64 --configuration "Release" --framework "net6.0" --urls "http://+:80" --self-contained
+dotnet publish -o /opt/publish --runtime linux-x64 --configuration "Release" --framework "net6.0" --self-contained
+cd /opt/publish
+./console --urls http://+:80
